135830e640fe1f336cd34ab7dec8b7b3028af25e
Jorge Roldan <roldanjrgl@gmail.com>
2025-03-07 16:37:17 -0500

    added comments

--- hf_bert_seq_class/sequence_classification.py
+++ hf_bert_seq_class/sequence_classification.py
@@ -18,9 +18,11 @@ def get_model_tokenizer(checkpoint: str, output_dir: str) -> (AutoTokenizer, Ber
         print(f"Model directory {output_dir} does not exist. It will be downloaded from Huggingface")
         os.makedirs(output_dir)
 
+        # Download model and tokenizer
         model = BertForSequenceClassification.from_pretrained(checkpoint)
         tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+        # Store model and tokenizer in output_dir
         model.save_pretrained(output_dir)
         tokenizer.save_pretrained(output_dir)
     else:
@@ -30,24 +32,30 @@ def get_model_tokenizer(checkpoint: str, output_dir: str) -> (AutoTokenizer, Ber
 
     return tokenizer, model
 
-# Setup tokenizer and model
-checkpoint = "nlptown/bert-base-multilingual-uncased-sentiment"
-output_dir = 'hf_bert_seq_class/model'
-tokenizer, model = get_model_tokenizer(checkpoint=checkpoint, output_dir=output_dir)
 
-# Stage 1: Pre-processing
-inputs = tokenizer("I really loved that movie", return_tensors="pt")
-for input, value in inputs.items():
-    print(f"{input:<15}: \t{value}")
+def main():
+    # Setup tokenizer and model
+    checkpoint = "nlptown/bert-base-multilingual-uncased-sentiment"
+    output_dir = 'hf_bert_seq_class/model'
+    tokenizer, model = get_model_tokenizer(checkpoint=checkpoint, output_dir=output_dir)
 
-# Stage 2: Model inference
-with torch.no_grad():
-    logits = model(**inputs).logits
-print(f"logits: \t{logits}")
+    # Stage 1: Pre-processing
+    inputs = tokenizer("I really loved that movie", return_tensors="pt")
+    for _input, value in inputs.items():
+        print(f"{_input:<15}: \t{value}")
 
-# Stage 3: Post-processing
-predictions = torch.nn.functional.softmax(logits, dim=-1)
-print(predictions)
-print(model.config.id2label)
-for id, label in model.config.id2label.items():
-    print(f"{label:<7}:\t{round(float(predictions[0][id]), 3)}")
+    # Stage 2: Model inference
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    print(f"logits: \t{logits}")
+
+    # Stage 3: Post-processing
+    predictions = torch.nn.functional.softmax(logits, dim=-1)
+    print(predictions)
+    print(model.config.id2label)
+    for _id, label in model.config.id2label.items():
+        print(f"{label:<7}:\t{round(float(predictions[0][_id]), 3)}")
+
+
+if __name__ == "__main__":
+    main()
