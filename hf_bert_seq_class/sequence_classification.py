de663272714b4955fd7d05cf7aecb04ed0422886
Jorge Roldan <roldanjrgl@gmail.com>
2025-03-07 16:43:12 -0500

    added comments and positive, negative examples

--- hf_bert_seq_class/sequence_classification.py
+++ hf_bert_seq_class/sequence_classification.py
@@ -1,3 +1,6 @@
+"""
+This script demonstrates the pipeline for sequence classification using Huggingface transformers.
+"""
 import os
 import torch
 from transformers import AutoTokenizer, BertForSequenceClassification
@@ -33,14 +36,15 @@ def get_model_tokenizer(checkpoint: str, output_dir: str) -> (AutoTokenizer, Ber
     return tokenizer, model
 
 
-def main():
-    # Setup tokenizer and model
-    checkpoint = "nlptown/bert-base-multilingual-uncased-sentiment"
-    output_dir = 'hf_bert_seq_class/model'
-    tokenizer, model = get_model_tokenizer(checkpoint=checkpoint, output_dir=output_dir)
-
+def run_pipeline(utterance: str, tokenizer, model: BertForSequenceClassification):
+    """ Run the pipeline for the sequence classification task
+        Args:
+            utterance: Input text
+            tokenizer: Tokenizer object
+            model: Model object
+    """
     # Stage 1: Pre-processing
-    inputs = tokenizer("I really loved that movie", return_tensors="pt")
+    inputs = tokenizer(utterance, return_tensors="pt")
     for _input, value in inputs.items():
         print(f"{_input:<15}: \t{value}")
 
@@ -55,6 +59,19 @@ def main():
     print(model.config.id2label)
     for _id, label in model.config.id2label.items():
         print(f"{label:<7}:\t{round(float(predictions[0][_id]), 3)}")
+
+
+def main():
+    # Setup tokenizer and model
+    checkpoint = "nlptown/bert-base-multilingual-uncased-sentiment"
+    output_dir = 'hf_bert_seq_class/model'
+    tokenizer, model = get_model_tokenizer(checkpoint=checkpoint, output_dir=output_dir)
+
+    # Positive review
+    run_pipeline(utterance="I really loved that movie", tokenizer=tokenizer, model=model)
+
+    # Negative review
+    run_pipeline(utterance="I hate very cold, and cloudy winter days", tokenizer=tokenizer, model=model)
 
 
 if __name__ == "__main__":
