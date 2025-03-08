6add2d78f789149e8c1dd0f9663c466b2309ed64
Jorge Roldan <roldanjrgl@gmail.com>
2025-03-08 13:18:17 -0500

    wip sequence_classification.py

--- hf_bert_seq_class/sequence_classification.py
+++ hf_bert_seq_class/sequence_classification.py
@@ -3,6 +3,7 @@ This script demonstrates the pipeline for sequence classification using Huggingf
 """
 import os
 import torch
+from typing import List
 from transformers import AutoTokenizer, BertForSequenceClassification
 
 
@@ -17,7 +18,7 @@ def get_model_tokenizer(checkpoint: str, output_dir: str) -> (AutoTokenizer, Ber
         tokenizer: Tokenizer object
         model: Model object
     """
-    os.makedirs(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
 
     # Download model and tokenizer
     model = BertForSequenceClassification.from_pretrained(checkpoint)
@@ -30,6 +31,21 @@ def get_model_tokenizer(checkpoint: str, output_dir: str) -> (AutoTokenizer, Ber
     return tokenizer, model
 
 
+def get_id_token_mapping(inputs, tokenizer) -> List[dict]:
+    """ Get the mapping between the token id and its respective token
+
+        Args:
+            inputs: Output of tokenizer containing input_ids
+            tokenizer: The tokenizer object
+    """
+    _mapping = []
+    id2token = {value: str(key) for key, value in tokenizer.vocab.items()}
+    input_ids = inputs.input_ids[0].tolist()
+    for token_id in input_ids:
+        _mapping.append({str(token_id): id2token.get(token_id)})
+    return _mapping
+
+
 def run_pipeline(utterance: str, tokenizer, model: BertForSequenceClassification):
     """ Run the pipeline for the sequence classification task
         Args:
@@ -37,22 +53,26 @@ def run_pipeline(utterance: str, tokenizer, model: BertForSequenceClassification
             tokenizer: Tokenizer object
             model: Model object
     """
-    print(f"\n{50*'='}\nRunning pipeline: \"{utterance}\"\n{50*'='}")
+    print(f"\n{50 * '='}\nRunning pipeline: \"{utterance}\"\n{50 * '='}")
 
     # Stage 1: Preprocessing
-    print(f"{50*'-'}\nStage 1: Preprocessing \n{50*'-'}")
+    print(f"{50 * '-'}\nStage 1: Preprocessing \n{50 * '-'}")
     inputs = tokenizer(utterance, return_tensors="pt")
     for _input, value in inputs.items():
         print(f"{_input:<15}: \n\t{value}")
 
+    print(f"\n** Additional details (token_id to token mapping) **")
+    mapping = get_id_token_mapping(inputs=inputs, tokenizer=tokenizer)
+    print(f"mapping: \n\t{mapping}")
+
     # Stage 2: Model inference
-    print(f"\n{50*'-'}\nStage 2: Model inference \n{50*'-'}")
+    print(f"\n{50 * '-'}\nStage 2: Model inference \n{50 * '-'}")
     with torch.no_grad():
         logits = model(**inputs).logits
     print(f"logits: \n\t{logits}")
 
     # Stage 3: Post-processing
-    print(f"\n{50*'-'}\nStage 3: Preprocessing \n{50*'-'}")
+    print(f"\n{50 * '-'}\nStage 3: Post-processing \n{50 * '-'}")
     predictions = torch.nn.functional.softmax(logits, dim=-1)
     print(f"probabilities: \n\t{predictions}")
     print(f"id2label: \n\t{model.config.id2label}")
